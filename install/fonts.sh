3cb76b2334faff7247ab3fd85abbb376e71c7c45
Alex Lourie <djay.il@gmail.com>
2025-07-18 21:52:44 +0930

    Fix jetbrains
    Signed-off-by: Alex Lourie <djay.il@gmail.com>

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,4 +1,4 @@
-paru -Sy --noconfirm --needed ttf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra nerd-fonts-jetbrains-mono
+paru -Sy --noconfirm --needed ttf-font-awesome noto-fonts noto-fonts-emoji noto-fonts-cjk noto-fonts-extra ttf-jetbrains-mono
 
 mkdir -p ~/.local/share/fonts
 
